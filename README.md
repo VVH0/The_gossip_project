35618af9d044b6f0ec1e39ef35f5d9dee4b261f1
VALEUR Virginie <valeurvirginie@macbook-pro-de-valeur.home>
2018-10-25 21:00:05 +0200

    gossip

---
+++ README.md
@@ -0,0 +1,27 @@
+# GOSSIP PROJECT
+
+L'objectif de ce projet était de réaliser une base de données à l'aide de Rails avec des modèles, des attributs et des interactions entre ces différents modèles.
+
+## 🐣 Pour commencer
+
+Cloner ce repository.
+
+## 🔧 Pré-requis
+
+Il sera nécessaire d'installer certains gems. Pour réaliser cette base de données, nous avons utilisé Ruby 2.5.3 (ou Ruby 2.5.1) ainsi que Rails 5.2.1. Veillez bien à avoir ces versions installées pour pouvoir utiliser cette base de données. Pour ce faire, rentrer dans le repository grâce à la commande :
+```
+cd gossip_project
+```
+
+Puis taper dans le Terminal :
+```
+bundle install
+```
+Et :
+```
+rails db:migrate
+```
+
+## 👉🏼 Auteurs 👈🏼
+
+Ce travail a été réalisé en pair-programming avec Anaïs Diologeant et Mustapha Ait Ouayhou.
---
+++ db/seeds.rb
@@ -0,0 +1,32 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  city = City.create!(name:Faker::Nation.capital_city,postal_code:Faker::Address.zip_code)
+end
+
+10.times do
+  user = User.create!(first_name:Faker::Name.first_name,last_name:Faker::Name.last_name,description:Faker::Lorem.sentence(10),email:Faker::Internet.email,age:Faker::Number.number(2),city_id:Faker::Number.between(City.first.id,City.last.id))
+end
+
+20.times do
+  gossip = Gossip.create!(title:Faker::Book.title,content:Faker::Community.quotes,user_id:Faker::Number.between(User.first.id,User.last.id))
+end
+
+10.times do
+  tag = Tag.create!(title:Faker::Color.color_name)
+end
+
+Tag.all.each do |tag|
+    tag.gossips << Gossip.find(
+      rand((Gossip.first.id)..(Gossip.last.id))
+    )
+  end
+
+private_messages = PrivateMessage.create!(recipient: User.find(rand((User.first.id)..(User.last.id))),sender:User.find(rand((User.first.id)..(User.last.id))),content:Faker::Community.quotes)
